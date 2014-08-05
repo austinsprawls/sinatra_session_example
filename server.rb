744e2a70a6e09402e5da27e42cd2a61d2f959941
vagrant <vagrant@student-dev-box>
2014-08-05 13:41:58 -0500

    added sign up capability

--- server.rb
+++ server.rb
@@ -3,11 +3,13 @@ require_relative 'lib/sesh.rb'
 
 set :sessions, true
 
+set :bind, "0.0.0.0"
+
 get '/' do
   if session['sesh_example']
     @user = Sesh.dbi.get_user_by_username(session['sesh_example'])
   end
-  
+
   erb :index
 end
 
@@ -23,6 +25,17 @@ post '/signin' do
   else
     "THAT'S NOT THE RIGHT PASSWORD!!!!"
   end
+end
+
+get '/signup' do
+  erb :signup
+end
+
+post '/signup' do
+  @user = Sesh::User.new(params[:username], params[:password])
+  @user.update_password(@user.password_digest)
+  Sesh.dbi.persist_user(@user)
+  erb :signup
 end
 
 get '/signout' do
